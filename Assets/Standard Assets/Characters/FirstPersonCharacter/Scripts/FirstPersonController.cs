639a92e4ecf0fdfb7ebad3bacc48284f6d79dedb
killtheverse <r.dev2000@gmail.com>
2020-10-06 20:59:24 +0530

    added map

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -63,35 +63,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         private void Update()
         {
             RotateView();
-            // the jump state needs to read here to make sure it is not missed
-            // if (!m_Jump)
-            // {
-            //     m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
-            // }
-
-            // if (!m_PreviouslyGrounded && m_CharacterController.isGrounded)
-            // {
-            //     StartCoroutine(m_JumpBob.DoBobCycle());
-            //     PlayLandingSound();
-            //     m_MoveDir.y = 0f;
-            //     m_Jumping = false;
-            // }
-            // if (!m_CharacterController.isGrounded && !m_Jumping && m_PreviouslyGrounded)
-            // {
-            //     m_MoveDir.y = 0f;
-            // }
-
-            // m_PreviouslyGrounded = m_CharacterController.isGrounded;
         }
-
-
-        private void PlayLandingSound()
-        {
-            m_AudioSource.clip = m_LandSound;
-            m_AudioSource.Play();
-            m_NextStep = m_StepCycle + .5f;
-        }
-
 
         private void FixedUpdate()
         {
@@ -114,13 +86,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
             {
                 m_MoveDir.y = -m_StickToGroundForce;
 
-                if (m_Jump)
-                {
-                    m_MoveDir.y = m_JumpSpeed;
-                    PlayJumpSound();
-                    m_Jump = false;
-                    m_Jumping = true;
-                }
             }
             else
             {
@@ -133,14 +98,6 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
             m_MouseLook.UpdateCursorLock();
         }
-
-
-        private void PlayJumpSound()
-        {
-            m_AudioSource.clip = m_JumpSound;
-            m_AudioSource.Play();
-        }
-
 
         private void ProgressStepCycle(float speed)
         {
@@ -211,12 +168,11 @@ namespace UnityStandardAssets.Characters.FirstPerson
             bool waswalking = true;
 
 #if !MOBILE_INPUT
-            // On standalone builds, walk/run speed is modified by a key press.
-            // keep track of whether or not the character is walking or running
+            //Sprinting is disabled
             m_IsWalking = true;
 #endif
-            // set the desired speed to be walking or running
-            speed = m_IsWalking ? m_WalkSpeed : m_RunSpeed;
+            // set the desired speed to walkspeed
+            speed = m_WalkSpeed;
             m_Input = new Vector2(horizontal, vertical);
 
             // normalize input if it exceeds 1 in combined length:
